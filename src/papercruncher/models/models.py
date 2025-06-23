fc9e4282f0845c17b2882afecfe5f54d37fb03e5
mluzu <luzuriagamariano@gmail.com>
2025-06-23 18:24:49 -0300

    base connector

--- src/papercruncher/models/models.py
+++ src/papercruncher/models/models.py
@@ -8,8 +8,8 @@ Base = declarative_base()
 
 class Paper(Base):
     __tablename__ = 'papers'
-    id        = Column(Integer, primary_key=True, autoincrement=True)
-    title     = Column(String, nullable=False, unique=True)
-    summary   = Column(String, nullable=False)
-    authors   = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False, unique=True)
+    summary = Column(String, nullable=False)
+    authors = Column(String, nullable=True)
     published = Column(DateTime, default=datetime.utcnow)
