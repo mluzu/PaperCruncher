09c28673b5f1ba75816a85bf68792b9db9596e25
mluzu <luzuriagamariano@gmail.com>
2025-06-23 17:19:03 -0300

    paper model

---
+++ src/papercruncher/models/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Paper(Base):
+    __tablename__ = 'papers'
+    id        = Column(Integer, primary_key=True, autoincrement=True)
+    title     = Column(String, nullable=False, unique=True)
+    summary   = Column(String, nullable=False)
+    authors   = Column(String, nullable=True)
+    published = Column(DateTime, default=datetime.utcnow)
+
+__all__ = ["Base", "Paper"]
